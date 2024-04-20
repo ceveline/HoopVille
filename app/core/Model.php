1b85f71602795adf3805e589a8cea39c92b55686
ceveline <cevelinevangelista@gmail.com>
2024-04-19 22:10:58 -0400

    Update Model.php

--- app/core/Model.php
+++ app/core/Model.php
@@ -9,7 +9,7 @@ class Model{
 	//$this is the reference of an object
 	public function __construct(){
 		$host = 'localhost';
-		$dbname = 'assignment2';
+		$dbname = 'hoopville';
 		$user = 'root';
 		$pass = '';
 		try { # MySQL with PDO_MYSQL
