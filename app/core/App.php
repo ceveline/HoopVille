27af6d99cd279360802b5a654367b8e2210c572d
%sereensaaida% <%sda.sereen@gmail.com%>
2024-04-22 13:21:11 -0400

    changes

--- app/core/App.php
+++ app/core/App.php
@@ -8,17 +8,37 @@ class App
 
     public function addRoute($url, $handler)
     {
+<<<<<<< HEAD
 =======
 class App {
     private $routes = [];
 
     public function addRoute($url,$handler){
 >>>>>>> parent of ed149ba (merged)
+=======
+>>>>>>> DenisBranch
+>>>>>>> parent of d2543ad (merged)
         $url = preg_replace('/{([^\/]+)}/', '(?<$1>[^\/]+)', $url);
         $this->routes[$url] = $handler;
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+    public function resolve($url){
+        $matches = [];
+        //one by one compare the url to resolve the route
+        foreach ($this->routes as $routePattern => $controllerMethod) {
+            if(preg_match("#^$routePattern$#", $url, $matches)){//match the route
+
+                // Filter named parameters
+                $namedParams = array_filter($matches,
+                    function($key) {
+                        return !is_numeric($key);
+                    }
+                    , ARRAY_FILTER_USE_KEY);
+=======
+>>>>>>> parent of d2543ad (merged)
     public function resolve($url)
     {
         $matches = [];
@@ -35,6 +55,7 @@ class App {
                     ,
                     ARRAY_FILTER_USE_KEY
                 );
+<<<<<<< HEAD
 =======
     public function resolve($url){
         $matches = [];
@@ -49,6 +70,9 @@ class App {
                     }
                     , ARRAY_FILTER_USE_KEY);
 >>>>>>> parent of ed149ba (merged)
+=======
+>>>>>>> DenisBranch
+>>>>>>> parent of d2543ad (merged)
 
                 return [$controllerMethod, $namedParams];
             }
@@ -57,11 +81,19 @@ class App {
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     function filtered($controllerInstance, $method)
     {
 =======
     function filtered($controllerInstance, $method){
 >>>>>>> parent of ed149ba (merged)
+=======
+    function filtered($controllerInstance, $method){
+=======
+    function filtered($controllerInstance, $method)
+    {
+>>>>>>> DenisBranch
+>>>>>>> parent of d2543ad (merged)
 
         //create an object that can get information about the controller
         $reflection = new \ReflectionClass($controllerInstance);
@@ -70,20 +102,34 @@ class App {
         $methodAttributes = $reflection->getMethod($method)->getAttributes();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         $attributes = array_merge($classAttributes, $methodAttributes);
 =======
         $attributes = array_merge($classAttributes,$methodAttributes);
 >>>>>>> parent of ed149ba (merged)
+=======
+        $attributes = array_merge($classAttributes,$methodAttributes);
+=======
+        $attributes = array_merge($classAttributes, $methodAttributes);
+>>>>>>> DenisBranch
+>>>>>>> parent of d2543ad (merged)
 
         foreach ($attributes as $attribute) {
             //instantiate the filter
             $filter = $attribute->newInstance();
             //run the filter and test if redirected
 <<<<<<< HEAD
+<<<<<<< HEAD
             if ($filter->redirected()) {
 =======
             if($filter->redirected()){
 >>>>>>> parent of ed149ba (merged)
+=======
+            if($filter->redirected()){
+=======
+            if ($filter->redirected()) {
+>>>>>>> DenisBranch
+>>>>>>> parent of d2543ad (merged)
                 return true;
             }
         }
@@ -92,6 +138,7 @@ class App {
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     function __construct()
     {
         //call the appropriate controller class and method to handle the HTTP Request
@@ -99,11 +146,31 @@ class App {
     function __construct(){
     	//call the appropriate controller class and method to handle the HTTP Request
 >>>>>>> parent of ed149ba (merged)
+=======
+    function __construct(){
+    	//call the appropriate controller class and method to handle the HTTP Request
+=======
+    function __construct()
+    {
+        //call the appropriate controller class and method to handle the HTTP Request
+>>>>>>> DenisBranch
+>>>>>>> parent of d2543ad (merged)
         //Routing version 1.0
 
         $url = $_GET['url'];
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+        include('app/routes.php');
+
+        [$controllerMethod, $namedParams] = $this->resolve($url);
+
+        if(!$controllerMethod){ return;  }
+
+        [$controller,$method] = explode(',', $controllerMethod);
+=======
+>>>>>>> parent of d2543ad (merged)
         include ('app/routes.php');
 
         [$controllerMethod, $namedParams] = $this->resolve($url);
@@ -113,6 +180,7 @@ class App {
         }
 
         [$controller, $method] = explode(',', $controllerMethod);
+<<<<<<< HEAD
 =======
         include('app/routes.php');
 
@@ -122,15 +190,25 @@ class App {
 
         [$controller,$method] = explode(',', $controllerMethod);
 >>>>>>> parent of ed149ba (merged)
+=======
+>>>>>>> DenisBranch
+>>>>>>> parent of d2543ad (merged)
 
         $controller = '\app\controllers\\' . $controller;
         $controllerInstance = new $controller();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         if ($this->filtered($controllerInstance, $method)) {
 =======
         if($this->filtered($controllerInstance, $method)){
 >>>>>>> parent of ed149ba (merged)
+=======
+        if($this->filtered($controllerInstance, $method)){
+=======
+        if ($this->filtered($controllerInstance, $method)) {
+>>>>>>> DenisBranch
+>>>>>>> parent of d2543ad (merged)
             return;
         }
 
