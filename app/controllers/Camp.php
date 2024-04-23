2f7dc37d6ca276c4a67d97bd0186656c6892d043
%sereensaaida% <%sda.sereen@gmail.com%>
2024-04-22 22:13:45 -0400

    Update Camp.php

--- app/controllers/Camp.php
+++ app/controllers/Camp.php
@@ -9,5 +9,33 @@ class Camp extends \app\core\Controller {
       
       $this->view('User/Camp/list', null, true);
     }
+
+    function insert(){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $guest = new \app\models\Guest();
+            $guest->first_name = $_POST['first-name'];
+            $guest->last_name = $_POST['last_name'];
+            $guest->date_of_birth = $_POST['dob'];
+
+
+            $camp = new \app\models\Camp(); //instance of camp class
+
+            $camp->camp_type = $_POST['camp_type'];
+            $camp->user_id = $_SESSION['user_id'];
+            $camp->guest_id = $_POST['guest-id']; //must get from guest isntance
+      
+            $camp->insert(); //inserting into db
+      
+            header('location:/User/Camp/list'); //redirecting user to the general review page
+          } else {
+            $this->view('User/Camp/list', null, true); 
+            
+          }
+    }
+
+    function getUserCamps(){
+
+    }
   
 }
