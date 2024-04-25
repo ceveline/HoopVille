c1ae0d64ba58e4e5f1986f0adf6143e209fc99fd
Ceveline <74314874+ceveline@users.noreply.github.com>
2024-04-25 11:46:04 -0400

    fixed create

--- app/views/Admin/Publication/view.php
+++ app/views/Admin/Publication/view.php
@@ -2,8 +2,8 @@
 <html lang="en">
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="/assets/styles/view_publication.css">
     <title>Main</title>
 </head>
@@ -13,14 +13,8 @@
         <div class="content">
             <div class="container">
 
-                <h1><?php echo $publication->title; ?></h1>
+                <div class="title"><h1><?php echo $publication->title; ?></h1></div>
 
-                <?php if (isset($_SESSION['admin_id'])): ?>
-                    <div class="edit-delete-buttons">
-                        <a href="/Publication/edit/<?php echo $publication->publication_id; ?>">Edit</a>
-                        <a href="/Publication/delete/<?php echo $publication->publication_id; ?>">Delete</a>
-                    </div>
-                <?php endif; ?>
                 <table class="table">
                     <tbody>
                         <tr>
@@ -30,15 +24,22 @@
                                         <p><?= $publication->text ?></p>
                                     </div>
                                     <div class="timestamp">
-                                        <p>Created on <?= $publication->timestamp ?></p>
+                                        <p>Posted on <?= $publication->timestamp ?></p>
                                     </div>
                                 </div>
                             </td>
                         </tr>
                     </tbody>
                 </table>
+
+                <div class="buttons">
+                    <?php if (isset($_SESSION['admin_id'])): ?>
+                        <div class="edit-btn"><a href="/Publication/edit/<?php echo $publication->publication_id; ?>">Edit</a></div>
+                        <div class="delete-btn"><a href="/Publication/delete/<?php echo $publication->publication_id; ?>">Delete</a></div>
+                    <?php endif; ?>
                 </div>
             </div>
+        </div>
     </div>
 </body>
 
