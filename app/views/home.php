384c24888a4b31c02f6c7f00707ea6b3881c9fa9
ceveline <cevelinevangelista@gmail.com>
2024-04-26 20:55:21 -0400

    main menu

--- app/views/home.php
+++ app/views/home.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/styles/home.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="background">
+        <div class="content animated animatedFadeInUp fadeInUp">
+            <div class="text">
+                <div class="title">
+                    <h1>HoopVille Performance Centre</h1>
+                </div>
+                <div class="subtitle">
+                    <h3>Take your game to the next level</h3>
+                </div>
+            </div>
+            <div class="buttons">
+                <div class="more-info-btn"><button>More info</button></div>
+                <div class="join-now-btn"><button>Join now</button></div>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
