14638cb38e231a669df8b36fd8bb552c497ade7a
%sereensaaida% <%sda.sereen@gmail.com%>
2024-05-10 12:39:34 -0400

    more

--- app/controllers/Camp.php
+++ app/controllers/Camp.php
@@ -10,7 +10,7 @@ class Camp extends \app\core\Controller {
     function buy(){
       if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        //$user_id = $_SESSION['user_id'];
+        $user_id = $_SESSION['user_id'];
 
         date_default_timezone_set('America/New_York');
 
@@ -64,8 +64,8 @@ class Camp extends \app\core\Controller {
     function listCamps(){
 
 
-      $userid = 1;
-      //$userid = $_SESSION['user_id'];
+   
+      $userid = $_SESSION['user_id'];
       $camp = new \app\models\Camp();
       $camp = $camp->listUserCamps($userid);
     }
